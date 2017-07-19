6348ee7fd0c659678b64a0ebaac03f76465303ed
Vadimatorik <deryabkinda@mail.ru>
2017-07-19 13:28:39 +0700

    Добавлен enum class на возвращаемое значение операции spi master 8bit.

--- mk_hardware_interfaces_spi.h
+++ mk_hardware_interfaces_spi.h
@@ -2,6 +2,11 @@
 
 #include <stdint.h>
 
+enum class EC_SPI_BASE_RESULT {
+    OK                  = 0,            // Операция успешна произведена.
+    TIME_OUT            = 1,            // Был произведён выход по истечении времени ожидания.
+};
+
 //**********************************************************************
 // SPI в режиме мастера, транзакции по 8 бит.
 //**********************************************************************
@@ -16,11 +21,11 @@ public:
     // length       -   длина посылки. length != 0! length >= 1!
     //
     // timeout_ms   -   если посылка не была передана в течении этого времени
-    //                  (в миллисекундах, то выходим с ошибкой).
+    //                  ( в миллисекундах ), то выходим с ошибкой.
     //
     // ЗАМЕЧАНИЕ: входной прием не ведется (входные данные просто теряются)!
     //**********************************************************************
-    virtual int tx ( const uint8_t* const  p_array_tx, const uint8_t& length, const uint32_t& timeout_ms ) const = 0;
+    virtual EC_SPI_BASE_RESULT tx ( const uint8_t* const  p_array_tx, const uint8_t& length, const uint32_t& timeout_ms ) const = 0;
 
     //**********************************************************************
     // p_array_tx   -   указатель на массив, который требуется передать
@@ -35,12 +40,12 @@ public:
     // length       -   длина посылки. length != 0! length >= 1!
     //
     // timeout_ms   -   если посылка не была передана в течении этого времени
-    //                  (в миллисекундах, то выходим с ошибкой).
+    //                  ( в миллисекундах ), то выходим с ошибкой.
     //
     // ЗАМЕЧАНИЕ: p_array_tx может быть равен p_array_rx. Тогда
     // принятые данные перезапишут массив исходящих данных!
     //**********************************************************************
-    virtual int tx ( const uint8_t* const  p_array_tx, uint8_t* p_array_rx, const uint16_t& length, const uint32_t& timeout_ms ) const = 0;
+    virtual EC_SPI_BASE_RESULT tx ( const uint8_t* const  p_array_tx, uint8_t* p_array_rx, const uint16_t& length, const uint32_t& timeout_ms ) const = 0;
 
     //**********************************************************************
     // p_item_tx    -   указатель на переменную, которая будет передаваться
@@ -50,11 +55,11 @@ public:
     //                  count != 0! count >= 1!
     //
     // timeout_ms   -   если посылка не была передана в течении этого времени
-    //                  (в миллисекундах, то выходим с ошибкой).
+    //                  ( в миллисекундах ), то выходим с ошибкой.
     //
     // Передаем 1 байт count раз.
     //**********************************************************************
-    virtual int tx_one_item ( const uint8_t* const  p_item_tx, const uint16_t& count, const uint32_t& timeout_ms ) const = 0;
+    virtual EC_SPI_BASE_RESULT tx_one_item ( const uint8_t* const  p_item_tx, const uint16_t& count, const uint32_t& timeout_ms ) const = 0;
 
     //**********************************************************************
     // p_array_rx   -   указатель на массив, в который будет
@@ -65,12 +70,12 @@ public:
     // length       -   количество принимаемых байт.
     //
     // timeout_ms   -   если посылка не была передана в течении этого времени
-    //                  (в миллисекундах, то выходим с ошибкой).
+    //                  ( в миллисекундах ), то выходим с ошибкой.
     //
     // value_out    -   значение, которое будет отправляться по TX
     //                  ( если будет использоваться ).
     //**********************************************************************
-    virtual int rx ( void* p_array_rx, const uint16_t& length, const uint32_t& timeout_ms, const uint8_t& out_value = 0xFF ) const = 0;
+    virtual EC_SPI_BASE_RESULT rx ( void* p_array_rx, const uint16_t& length, const uint32_t& timeout_ms, const uint8_t& out_value = 0xFF ) const = 0;
 
     virtual ~spi_base() {}
 };
